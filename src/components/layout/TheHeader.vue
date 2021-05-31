81f20d21e831063bde1bd008fb6a6bb60c2663d0
trymvalebjorg <trymvalebjorg@gmail.com>
2021-05-31 10:00:09 +0200

    Added bootstrap icons

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,36 +1,10 @@
 <template>
-  <div class="px-3 py-2 bg-dark text-white">
-    <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
-          <h3>{{ pageTitle }}</h3>
-        </a>
-        <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
-          <li>
-            <router-link to="/" class="nav-link text-secondary">
-              <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#home"></use></svg>
-              Home
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/manual" class="nav-link text-secondary">
-              <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#home"></use></svg>
-              Smart Manual
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/report" class="nav-link text-secondary">
-              <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#home"></use></svg>
-              Report
-            </router-link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
+  <TheNavbar pageTitle="Bright"></TheNavbar>
 </template>
 
 <script>
+import TheNavbar from './TheNavbar'
+
 export default {
   name: 'TheHeader',
   props: {
@@ -39,5 +13,6 @@ export default {
     },
   },
   setup() {},
+  components: { TheNavbar },
 }
 </script>
---
+++ src/components/layout/TheNavbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="px-3 py-2 bg-dark text-white">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+          <h3>{{ pageTitle }}</h3>
+        </a>
+        <ul class="nav d-flex col-12 col-lg-auto my-2 justify-content-center align-content-center my-md-0 text-small">
+          <li>
+            <router-link to="/" class="nav-link text-secondary">
+              <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#home"></use></svg>
+              Home
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/manual" class="nav-link text-secondary">
+              <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#home"></use></svg>
+              Smart Manual
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/report" class="nav-link text-secondary">
+              <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#home"></use></svg>
+              Report
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheNavbar',
+  props: {
+    pageTitle: {
+      type: String,
+    },
+  },
+  setup() {},
+}
+</script>
+
+<style></style>
