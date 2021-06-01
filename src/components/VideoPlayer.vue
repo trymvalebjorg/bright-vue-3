d0ba486c3b2ad6d300d11502a38c2195cf0cfcac
TrondEinar <trond-einar98@hotmail.no>
2021-06-01 13:12:43 +0200

    VideoPlayer component

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,18 @@
+<template>
+ <div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item video" src="https://www.youtube.com/embed/PK7g180smjo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div> 
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.video {
+    width: 1000px;
+    height: 635px;
+}
+</style>
--- src/views/ELearning.vue
+++ src/views/ELearning.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
       <h3>E-Learning</h3>
+      <video-player></video-player>
   </div>
 </template>
 
 <script>
+import VideoPlayer from '../components/VideoPlayer.vue'
 export default {
-
+  components: { VideoPlayer },
 }
 </script>
 
