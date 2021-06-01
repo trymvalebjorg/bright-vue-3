8755b825e5e6a0561ecc531ae4a5cbc1cc46e0be
Johbra96 <61549336+Johbra96@users.noreply.github.com>
2021-06-01 16:42:42 +0200

    send report button

--- src/views/Report.vue
+++ src/views/Report.vue
@@ -2,10 +2,28 @@
   <div>
     <h3>Report</h3>
   </div>
+  <router-link to="/" class="btn btn-primary rounded-pill ps-4 report-btn">Send report 
+    <BIconArrowRightCircle width="2rem" height="1.5rem"/></router-link>
 </template>
 
 <script>
-export default {}
+import { BIconArrowRightCircle } from 'bootstrap-icons-vue'
+export default {
+  components: {
+    BIconArrowRightCircle
+  }
+}
 </script>
 
-<style></style>
+<style scoped lang="scss" >
+@import '../assets/styles/custom.scss';
+.report-btn{
+  color: white;
+  border: 0px;
+}
+
+.report-btn:hover{
+  color: $success;
+  background-color: $primary;
+}
+</style>
