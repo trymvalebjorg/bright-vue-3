9651ae5b527c31c84d2c74696cf921310787cc1e
Elise Easter Christiansen <eliseeaster@Elises-MacBook-Pro.local>
2021-06-02 08:02:42 +0200

    changet layout thankyoupage

--- src/views/ThankyouPage.vue
+++ src/views/ThankyouPage.vue
@@ -1,13 +1,41 @@
 <template>
-  <div>
-    <h3>Thank you</h3>
-    <p>Your report has been submitted</p>
-    
+<div class="container">
+  <div class="row d-flex justify-content-center ">
+    <BIconCheck class="col-4 text-success fs-1 text" />
+    <div class="col-7" >
+        <div class="row">
+            <div class="col fs-3 text">
+                Thank you
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col fs-5 text mt-1">
+                Your report has been submitted
+            </div>
+        </div>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
-export default {}
+
+import { BIconCheck } from 'bootstrap-icons-vue'
+export default {
+  components: {BIconCheck},
+  props: {
+    // ...
+  }
+  // ...
+}
 </script>
 
-<style></style>
+<style>
+
+.thankYouP{
+    display: flex;
+
+}
+
+</style>
